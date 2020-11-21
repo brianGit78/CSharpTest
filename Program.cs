7faa7e9a930a32a6585f3a113c0e3ad7fbaebbc0
Brian ONeil <brian@netadmin.com>
2020-11-20 21:42:45 -0700

    update from first string lab

--- Program.cs
+++ Program.cs
@@ -6,8 +6,20 @@ namespace myApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+
+            string firstName = "Brian";
+            string greeting = "Hello";
+            string message = $"{greeting} {firstName}";
+
+            Console.WriteLine(message + "!");
+            
             Console.WriteLine("The current time is: " + DateTime.Now);
+            
+            string projectName = "Test123";
+            Console.WriteLine($@"C:\output\{projectName}\Data");
+
+
         }
     }
 }
